1ec7f76b55193eeae82af3270e842c5ccbe13c38
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-10-26 18:39:51 +0300

    GW-03: Добавил библиотеку SwiftLint
    **Что сделано**
    
    - Инициализовал CocoaPods.
    - Подключил библиотеку SwiftLint.
    - Убрал варнинги в коде.

--- SpaceX/SpaceX/Supporting files/AppDelegate.swift
+++ SpaceX/SpaceX/Supporting files/AppDelegate.swift
@@ -9,10 +9,11 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let rootVC = ViewController()
         window?.rootViewController = UINavigationController(rootViewController: rootVC)
@@ -20,4 +21,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
