0b19fd5323d2cffe6454753192a5e98e07627cfe
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-10-26 17:50:05 +0300

    GW-03: Удалил Storyboard и SceneDelegate
    **Что сделано**
    
    - Удалил storyboard.
    - Удалил SceneDelegate.
    - Удалил лишние ключи из info.plist.
    - Добавил папку Supporting files.

---
+++ SpaceX/SpaceX/Supporting files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SpaceX
+//
+//  Created by Ibragim Akaev on 26/10/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootVC = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: rootVC)
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
