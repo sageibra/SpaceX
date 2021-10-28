98deb39dac08bd11cf062172972eb94ef07da006
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-10-28 21:56:48 +0300

    GW-11: Создал модель Rocket
    **Что сделано**
    
    - Создана модель Rocket.

---
+++ SpaceX/SpaceX/Model/Rocket.swift
@@ -0,0 +1,22 @@
+//
+//  Rocket.swift
+//  SpaceX
+//
+//  Created by Ibragim Akaev on 28/10/2021.
+//
+
+import Foundation
+
+// MARK: - Rocket
+struct Rocket: Decodable {
+    let active: Bool
+    let costPerLaunch: Int
+    let successRatePct: Int
+    let country: String
+    let company: String
+    let flickrImages: [String]
+    let wikipedia: String
+    let rocketDescription: String
+    let rocketName: String
+    let rocketType: String
+}
