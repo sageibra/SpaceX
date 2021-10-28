424d504dbbcd33738f327baf8a9238862c1d6e1b
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-10-28 21:54:40 +0300

    GW-10: Создал модель Launch
    **Что сделано**
    
    - Создана модель Launch.
    - Создана модель LaunchSite.
    - Создана модель Links.

---
+++ SpaceX/SpaceX/Model/Launch.swift
@@ -0,0 +1,30 @@
+//
+//  Launch.swift
+//  SpaceX
+//
+//  Created by Ibragim Akaev on 28/10/2021.
+//
+
+import Foundation
+
+struct Launch: Decodable  {
+    let missionName: String
+    let launchDateLocal: Date
+    let rocket: Rocket
+    let launchSite: LaunchSite
+    let launchSuccess: Bool?
+    let links: Links
+    let details: String?
+}
+
+// MARK: - LaunchSite
+struct LaunchSite: Decodable {
+    let siteNameLong: String
+}
+
+// MARK: - Links
+struct Links: Decodable {
+    let missionPatch: String?
+    let articleLink: String?
+    let videoLink: String?
+}
