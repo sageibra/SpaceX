a09d66fc587325e060adc503cfe82b1022d5811d
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-10-28 22:09:58 +0300

    GW-14: Добавил класс для обработки ошибок сети
    **Что сделано**
    
    - Создал класс NetworkServiceError и описал в нем ошибки сети.

---
+++ SpaceX/SpaceX/Services/Network/NetworkServiceError.swift
@@ -0,0 +1,24 @@
+//
+//  Launch.swift
+//  SpaceX
+//
+//  Created by Ibragim Akaev on 28/10/2021.
+//
+
+enum NetworkServiceError: Error, CustomStringConvertible {
+    case nonHTTPResponse
+    case requestFailed(Int)
+    case serverError(Int)
+    case networkError(Error)
+    case decodingError(Error)
+    
+    var description: String {
+        switch self {
+        case .nonHTTPResponse: return "Non-HTTP response received"
+        case .requestFailed(let status): return "Received HTTP \(status)"
+        case .serverError(let status): return "Server Error - \(status)"
+        case .networkError(let error): return "Failed to load the request: \(error)"
+        case .decodingError(let decError): return "Failed to process response: \(decError)"
+        }
+    }
+}
