533f8ad1cedd6a931e6a79409908b22e01147a9f
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-10-28 21:45:54 +0300

    GW-06: Добавил enum для изображений
    **Что сделано**
    
    - Создал enum для изображений.
    - Добавил системные иконки для таббара и ячеек.

---
+++ SpaceX/SpaceX/Resources/Images/Images.swift
@@ -0,0 +1,20 @@
+//
+//  Images.swift
+//  SpaceX
+//
+//  Created by Ibragim Akaev on 28/10/2021.
+//
+
+import UIKit
+
+enum Images {
+    
+    static let launchesIcon = UIImage(systemName: "flame")
+    static let rocketIcon = UIImage(named: "shuttle")
+    static let favoriteIcon = UIImage(systemName: "heart.circle")
+
+    static let failure = UIImage(systemName: "x.circle")
+    static let success = UIImage(systemName: "checkmark.circle")
+    static let calendar = UIImage(systemName: "calendar")
+    static let location = UIImage(systemName: "location.square")
+}
