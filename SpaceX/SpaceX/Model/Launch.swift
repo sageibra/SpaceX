f9d14145c5d23f46431d17d6769acf7cd5bb7657
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-11-05 09:16:23 +0300

    GW-34: Внесены изменения в модель Launch
    **Причина изменений**
    
    - Для работы с новой версией URL.
    
    **Что сделано**
    
    - Удалены не нужные свойства.
    - Добавлены новые свойства.

--- SpaceX/SpaceX/Model/Launch.swift
+++ SpaceX/SpaceX/Model/Launch.swift
@@ -8,30 +8,51 @@
 import Foundation
 
 struct Launch: Decodable {
-    let missionName: String
-    let launchDateUnix: Int
-    let flightNumber: Int
-    let rocket: RocketInfo
-    let launchSite: LaunchSite
-    let launchSuccess: Bool?
     let links: Links
+    let rocket: String
+    let success: Bool?
     let details: String?
-}
+    let launchpad: String
+    let flightNumber: Int
+    let name: String
+    let dateUnix: Int
+    let upcoming: Bool
 
-// MARK: - LaunchSite
-struct LaunchSite: Decodable {
-    let siteName: String
+    var rocketInfo: Rocket {
+        switch rocket {
+        case "5e9d0d95eda69955f709d1eb": return Rocket.falcon1
+        case "5e9d0d95eda69973a809d1ec": return Rocket.falcon9
+        case "5e9d0d95eda69974db09d1ed": return Rocket.falconHeavy
+        default: return Rocket.unknown
+        }
+    }
+
+    var launchpadInfo: Launchpad {
+        switch launchpad {
+        case "5e9e4501f509094ba4566f84": return Launchpad.ccsfs
+        case "5e9e4502f509092b78566f87": return Launchpad.vafb
+        case "5e9e4502f509094188566f88": return Launchpad.ksc
+        case "5e9e4502f5090995de566f86": return Launchpad.kwajaleinAtoll
+        default: return Launchpad.unknown
+        }
+    }
 }
 
 // MARK: - Links
 struct Links: Decodable {
-    let missionPatch: String?
-    let articleLink: String?
-    let videoLink: String?
+    let patch: Patch
+    let flickr: Flickr
+    let webcast: String?
+    let article: String?
+    let wikipedia: String?
 }
 
-// MARK: - RocketInfo
-struct RocketInfo: Decodable {
-    let rocketName: String
-    let rocketType: String
+// MARK: - Flickr
+struct Flickr: Decodable {
+    let original: [String]
+}
+
+// MARK: - Patch
+struct Patch: Decodable {
+    let large: String?
 }
