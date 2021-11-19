16ceba835ec88a00934f1ee33cae3bc0ccf600e3
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-11-19 14:06:28 +0300

    GW-96: Создан View для модуля
    **Что сделано**
    
    - Создан SafariViewController

---
+++ SpaceX/SpaceX/Screens/Safari/View/SafariViewController.swift
@@ -0,0 +1,10 @@
+//
+//  SafariViewController.swift
+//  SpaceX
+//
+//  Created by Ibragim Akaev on 29/10/2021.
+//
+
+import SafariServices
+
+final class SafariViewController: SFSafariViewController {}
