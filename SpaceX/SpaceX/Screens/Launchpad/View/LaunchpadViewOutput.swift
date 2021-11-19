4174aff8bdbeab1036f8ad8e3e8bd89d8d4f363d
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-11-19 13:59:43 +0300

    GW-88: Создан View для модуля
    **Что сделано**
    
    - Создан LaunchpadViewController
    - Создан LaunchpadViewInput
    - Создан LaunchpadViewOutput

---
+++ SpaceX/SpaceX/Screens/Launchpad/View/LaunchpadViewOutput.swift
@@ -0,0 +1,12 @@
+//
+//  LaunchpadViewOutput.swift
+//  SpaceX
+//
+//  Created by Ibragim Akaev on 16/11/2021.
+//  Copyright © 2021 Chetech. All rights reserved.
+//
+
+protocol LaunchpadViewOutput {
+    func viewLoaded()
+    func dismissModule()
+}
