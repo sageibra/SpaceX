3b9277c2b243a27d74b67211591647f59831a847
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-11-22 16:27:05 +0300

    GW-106: Добавил метод загрузки в AppDelegate
    **Что сделано**
    
    - Вызвал метод load для загрузки в AppDelegate

--- SpaceX/SpaceX/AppDelegate.swift
+++ SpaceX/SpaceX/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-
+        container.dataBase.coreDataStack.load()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UINavigationController(
             rootViewController: container.makeRootViewController()
